08659b86907a480b048ceb739eefe59049192e26
Avishrant Sharma <avishrants@gmail.com>
2020-08-22 22:09:05 +0530

    Nightly update push

--- scripts/libs/lib_fetch.py
+++ scripts/libs/lib_fetch.py
@@ -5,10 +5,32 @@ class Fetcher(object):
     response = ""
     n_response = {}
     def __init__(self, link):
+        print("Fetching")
         self.link = link
         Fetcher.response = requests.get(url = self.link)
+        print("Done")
 
     def extract(self):
         soup = BeautifulSoup(self.response.content, 'html5lib')
-        lst = soup.find_all('table')
-        return lst
+        self.lst = soup.find_all('table')
+        return self.lst
+
+    def jsonify(self, index, order="row"):
+        if order == "row":
+            keys=[]
+            result = {}
+            rows = self.lst[index].find_all('tr')
+            for i in range(len(rows)):
+                element = rows[i].find_all('td')
+                for j in range(len(element)):
+                    if i == 0:
+                        keys.append(element[j].get_text())
+            print(keys)
+
+        elif order == "column":
+            columns = self.lst[index].find_all('tr')
+            print(columns)
+            
+        else:
+            return "Invalid Arguments"
+        return "Hola"
--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,5 @@
 from libs.lib_fetch import Fetcher
 # Alpha Stage
-obj = Fetcher('https://www.w3schools.com/html/html_tables.asp')
-print(obj.extract())
+obj = Fetcher('http://127.0.0.1:8000/tmp/')
+array = obj.extract()
+print(obj.jsonify(0), "row")
